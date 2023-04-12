ae5fff65b0e184a4fc5d3c4df2b3f0adcae756ad
justinnonso05 <123962732+justinnonso05@users.noreply.github.com>
2023-04-12 13:45:21 +0100

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,19 @@
-function menu() {
+function show() {
   document.getElementById('menu').classList.toggle('active')
   document.getElementById('bars').classList.toggle('active')
   document.getElementById('times').classList.toggle('active')
 }
+
+const menu = document.querySelector('.menu');
+const links = document.querySelectorAll('.menu a');
+
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('active');
+    document.getElementById('bars').classList.toggle('active');
+    document.getElementById('times').classList.toggle('active');
+    //times.classList.remove('active');
+    //bars.classList.add('show')
+  });
+});
+
